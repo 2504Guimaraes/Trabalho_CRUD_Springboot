d5c0c767ab81447feac67a3bfa02e9440a050f1d
2504Guimaraes <guimaraes.ivan@hotmail.com>
2020-12-02 02:24:41 -0300

    Testando modelo aluno

--- src/main/java/com/classes/model/Aluno.java
+++ src/main/java/com/classes/model/Aluno.java
@@ -1,4 +1,16 @@
 package com.classes.model;
 
 public class Aluno {
+
+    private int cd_aluno;
+    private String nm_aluno;
+    private String sg_sexoaluno;
+    private int cd_curso;
+    private double qt_notaaluno;
+    private int qt_idade;
+
+    public Aluno() {}
+
+    // Antes de definir outros contrutores, setters ou getters
+    // irei confirmar se esse modelo já satisfaz a view
 }
