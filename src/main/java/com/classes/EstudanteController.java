e2a9426330017f43e6785cf6e29e2af2cdef942b
2504Guimaraes <guimaraes.ivan@hotmail.com>
2020-12-02 02:04:02 -0300

    Configurando getMapping gerarNovoObjAluno
    Conectando o futuro modelo de obj. Aluno a minha view por meio da
    minha variável "novo aluno". Esse obj seria criado sempre q fossemos
    a pág. criarAluno.html

--- src/main/java/com/classes/EstudanteController.java
+++ src/main/java/com/classes/EstudanteController.java
@@ -1,4 +1,16 @@
 package com.classes;
 
+import com.classes.model.Aluno;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class EstudanteController {
+
+    @GetMapping("/")
+    public String gerarNovoObjAluno(Model model) {
+        model.addAttribute("novo_aluno", new Aluno());
+        return "criarAluno";
+    }
 }
